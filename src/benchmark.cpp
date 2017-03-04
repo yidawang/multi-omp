e8982f1208cb7a43cab89ca7bb6d8dd77e07b442
Yida Wang <yidawa@gmail.com>
2017-03-03 23:52:51 -0800

    add low-level omp affinity

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -7,19 +7,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <glog/logging.h>
+#include <omp.h>
 
-void f1(int tid, size_t num_loops, int iter)
-{
+void f1(int tid, size_t num_loops, int iter) {
+    int threads;
+#pragma omp parallel
+    {
+    threads = omp_get_num_threads();
+    }
+    printf("%d\n", threads);
+    kmp_affinity_mask_t mask;
     float* arr = new float[num_loops];
     float* result = new float[num_loops];
-    int count = 0;
+    size_t count = 0;
     float compiler_treat;
+    kmp_create_affinity_mask(&mask);
+    int max_proc = kmp_get_affinity_max_proc();
+    for(int i=tid*threads;i<(tid+1)*threads; ++i) {
+        CHECK_LT(max_proc-1-i, max_proc);
+        CHECK_EQ(kmp_set_affinity_mask_proc(max_proc-1-i, &mask), 0);
+    }
+# pragma omp parallel num_threads(threads)
+    {
+        // set affinity for the spawned threads
+        CHECK_EQ(kmp_set_affinity(&mask), 0);
+    }
     while (count++ < iter) {
 #pragma omp parallel for
         for (size_t i = 0; i < num_loops; i++) {
             result[i] = arr[i] * 2.0;
         }
-        compiler_treat = result[iter];
+        compiler_treat = result[iter%num_loops];
         LOG(INFO) << sformat("in thread %d compiler_treat %.2f", tid, compiler_treat);
     }
     delete [] arr;
@@ -30,11 +48,11 @@ void f1(int tid, size_t num_loops, int iter)
 int main(int argc, char** argv)
 {
     size_t num_loops = 1000000000;
-    int iter = 100;
+    size_t iter = 100;
     int inter_threads = atoi(argv[1]);
     //int intra_threads = int(argv[2]);
     ctpl::thread_pool pool(inter_threads);
-    for (int i=0; i<inter_threads; i++) pool.push(f1, num_loops, iter);
+    for (int i=0; i<inter_threads; i++) pool.push(f1, num_loops*(i+1), iter);
 
     //float* result = f1(num_loops, iter);
     //LOG(INFO) << sformat("some results %.2f %.2f %.2f", result[0], result[10000], result[10000000]);
